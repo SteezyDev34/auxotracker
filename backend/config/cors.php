8dfda6f1f240f5d79857cda76227732a18340d88
Steeven PECOME | La Jungle <steeven@lajungle.fr>
2025-08-10 16:56:07 +0200

    fix(cors): add localhost:5174 to allowed origins
    refactor(docker): update network configuration and port mapping

--- backend/config/cors.php
+++ backend/config/cors.php
@@ -16,7 +16,7 @@ return [
 
     'paths' => ['api/*', 'sanctum/csrf-cookie'],
     'allowed_methods' => ['*'],
-    'allowed_origins' => ['http://auxotracker.lan', 'https://auxotracker.lan', 'http://localhost:5173'],
+    'allowed_origins' => ['http://auxotracker.lan', 'https://auxotracker.lan', 'http://localhost:5173', 'http://localhost:5174'],
     'allowed_origins_patterns' => [],
     'allowed_headers' => ['*'],
     'exposed_headers' => [],
